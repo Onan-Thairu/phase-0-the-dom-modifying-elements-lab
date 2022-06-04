10b30e11b6be16cf682eabfe8cc3142904d335b5
OnanThairu <thairuonan@gmail.com>
2022-06-04 15:09:07 +0300

    creat and insert

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// let element = document.createElement('h1')
+// element.id = "victory"
+// document.body.append(element)
+
+let newHeader = document.getElementById('victory')
+
+newHeader.textContent = 'Onan is the GOAT!'
